9fc6e1af936184d03199efa3bb17d05a62a0e4c6
Noura Loudani <contact.nouraloudani@gmail.com>
2020-10-30 22:28:09 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,11 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
-def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
-  else
+def snake_it_up (string)
+  if string[0] = "s"
+    str = ""
+    10.times{str += "s"}
+    str + string
+  else 
     string
   end
 end
