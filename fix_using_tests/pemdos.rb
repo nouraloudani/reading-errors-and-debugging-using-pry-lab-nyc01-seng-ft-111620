a832edd7dfb56596dc59bfdbaa749513bd82a2bf
Noura Loudani <contact.nouraloudani@gmail.com>
2020-10-30 22:27:23 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 def snake_it_up (string)
   if string[0] = "s"
-    return 10.times{puts "s"} + string
+    str = ""
+    return 10.times{str += "s"} + string
   else 
     string
   end
